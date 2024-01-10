7ca070518b9dd590bbab5c52ccd351c0d337731d
KaunivaJulia <151840241+KaunivaJulia@users.noreply.github.com>
2024-01-10 14:38:02 +0300

    Add files via upload

---
+++ CalendarServiceTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace TestProject1
+{
+    public class CalendarServiceTests
+    {
+        [Fact]
+        public void IsLeapYear_ReturnsTrueForLeapYear()
+        {
+
+            var mockContext = new Mock<AppDbContext>();
+            var calendarService = new CalendarService(mockContext.Object);
+            int year = 2024;
+            bool result = calendarService.IsLeapYear(year);
+            Assert.True(result);
+        }
+
+
+        [Fact]
+        public void CalcIntervalLength_ReturnsCorrectIntervalLength()
+        {
+            var mockDbContext = new Mock<AppDbContext>();
+            var calendarService = new CalendarService(mockDbContext.Object);
+            var from = new DateOnly(2022, 1, 1);
+            var to = new DateOnly(2022, 1, 10);
+            var result = calendarService.CalcIntervalLength(from, to);
+
+            Assert.Equal(9, result);
+        }
+
+        [Fact]
+        public void GetDayOfWeek_ReturnsCorrectDayOfWeek()
+        {
+            var mockDbContext = new Mock<AppDbContext>();
+            var calendarService = new CalendarService(mockDbContext.Object);
+            var date = new DateOnly(2022, 1, 1);
+
+            var result = calendarService.GetDayOfWeek(date);
+
+            Assert.Equal("Saturday", result);
+        }
+
+        [Fact]
+        public void Dates_ReturnsCorrectDates()
+        {
+
+            var mockDbContext = new Mock<AppDbContext>();
+            var calendarService = new CalendarService(mockDbContext.Object);
+            mockDbContext.Setup(db => db.Dates).Returns(new List<DateOnly> { new DateOnly(2022, 1, 1), new DateOnly(2022, 1, 10) });
+
+            var result = calendarService.Dates;
+
+            Assert.Contains(new DateOnly(2022, 1, 1), result);
+            Assert.Contains(new DateOnly(2022, 1, 10), result);
+        }
+    }
+}
+
---
+++ DateOnlyTypeConverterTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace TestProject1
+{
+
+    public class DateOnlyTypeConverterTests
+    {
+        [Fact]
+        public void CanConvertFrom_String_ReturnsTrue()
+        {
+
+            var converter = new DateOnlyTypeConverter();
+            bool result = converter.CanConvertFrom(null, typeof(string));
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ConvertFrom_ValidString_ReturnsDateOnly()
+        {
+
+            var converter = new DateOnlyTypeConverter();
+            string input = "2022-01-01";
+
+
+            var result = converter.ConvertFrom(null, null, input);
+
+
+            Assert.InstanceOf<DateOnly>(result);
+            Assert.Equal(new DateOnly(2022, 1, 1), result);
+        }
+
+        [Fact]
+        public void CanConvertTo_String_ReturnsTrue()
+        {
+
+            var converter = new DateOnlyTypeConverter();
+
+
+            bool result = converter.CanConvertTo(null, typeof(string));
+
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ConvertTo_DateOnly_ReturnsValidString()
+        {
+
+            var converter = new DateOnlyTypeConverter();
+            DateOnly date = new DateOnly(2022, 1, 1);
+
+
+            var result = converter.ConvertTo(null, null, date, typeof(string));
+
+
+            Assert.IsInstanceOf<string>(result);
+            Assert.Equal("2022-01-01", result);
+        }
+    }
+    }
